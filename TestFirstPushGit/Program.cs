af5b3730ca47f35116fb1d4b4d8ff64064a4acc6
Kabir Knight <kabzzy@gmail.com>
2018-01-28 13:42:39 +0530

    Add project files.

---
+++ TestFirstPushGit/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace TestFirstPushGit
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Test Git Push FIrst App");
+        }
+    }
+}
